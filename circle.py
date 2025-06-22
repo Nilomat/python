d7b27283ee50b7a6d075fd96651934cfe034cbed
Nilomat_studios <163658310+Nilomat@users.noreply.github.com>
2025-06-22 22:05:26 +0200

    circle tool

---
+++ circle.py
@@ -0,0 +1,13 @@
+pi = 3.141592654
+while 1 == 1:
+    print("---------------------------")
+    print("trage den R des kreises ein")
+    print("---------------------------")
+    R = int(input(str("             #")))
+    print("---------------------------")
+    D = R * 2
+    inhalt = pi * (R*R)
+    umfang = pi * D
+    print("inhalt =",inhalt)
+    print("umfang =",umfang)
+    print("---------------------------")
